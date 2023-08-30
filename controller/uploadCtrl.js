b0211a7ac2c0d109766114f8686022db2af1df93
asifullesan <asifullesan345@gmail.com>
2023-08-31 01:47:43 +0600

    renew

--- controller/uploadCtrl.js
+++ controller/uploadCtrl.js
@@ -6,7 +6,7 @@ const {
   cloudinaryDeleteImg,
 } = require("../utils/cloudinary");
 const uploadImages = asyncHandler(async (req, res) => {
-  console.log(req.files);
+  // console.log(req.files);
   try {
     const uploader = (path) => cloudinaryUploadImg(path, "images");
     const urls = [];
